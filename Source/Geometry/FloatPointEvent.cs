8ba726b1b1837ee4974ecb7ef759702abf05bd8e
Daniel Patterson <danielanywhere@users.noreply.github.com>
2025-03-02 09:38:58 -0600

    License Update

--- Source/Geometry/FloatPointEvent.cs
+++ Source/Geometry/FloatPointEvent.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright (c). 1997 - 2025 Daniel Patterson, MCSD (danielanywhere).
  * 
  * This program is free software: you can redistribute it and/or modify
@@ -15,6 +15,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  * 
  */
+
 using System;
 using System.Collections.Generic;
 using System.Text;
